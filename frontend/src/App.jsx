4462c1cc9f75ed4eb284819c19c0066c8985ac0f
Hari Varma <harigowtham458@gmail.com>
2025-09-03 14:56:29 +0530

    Setup for the frontend with tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+     <div className='text-red-400'>
+      en peyar hari varma
+     </div>
+    </>
+  )
+}
+
+export default App;

e144d35fde010c8d447725947663207b0475b78f
bharathkumar205 <bharath432005@gmail.com>
2025-09-09 21:44:26 +0530

    Admin page was created

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import AdminPage from './components/admin/Admin.jsx'
 import './index.css'
 import App from './App.jsx'
-import  {BrowserRouter}  from 'react-router-dom'
+import  {BrowserRouter ,Route ,Routes}  from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )

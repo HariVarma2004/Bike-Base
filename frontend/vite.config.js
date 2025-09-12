74cc8076186e98a55db62772f43d22c1315c4c68
bharathkumar205 <bharath432005@gmail.com>
2025-09-12 12:58:27 +0530

    Something i Did but i don't know

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss() ,react()]
+  plugins: [ tailwindcss() ,react()],
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "unsafe-none",
+      "Cross-Origin-Resource-Policy": "cross-origin",
+      "Cross-Origin-Opener-Policy": "same-origin"
+    }
+  }
   
 })
+

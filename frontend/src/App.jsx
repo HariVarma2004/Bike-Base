55bd9eec4386f8812ee6691ad5ebd50dd7d6e578
bharathkumar205 <bharath432005@gmail.com>
2025-09-12 15:06:06 +0530

    Something Something

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 // App.jsx
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout.jsx";
 import AdminLayout from "./components/admin/AdminLayout.jsx";
 import AddBike from "./components/admin/AddBike.jsx";
@@ -15,179 +15,192 @@ import UserDashboard from "./components/UserDashboard";
 import AdminDashboard from "./components/AdminDashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import EditBike from "./components/admin/EditBike.jsx";
+import EditUser from "./components/admin/EditUser.jsx";
 
-// Import the blog-related components
+// Blog components
 import BlogList from "./components/Blog/BlogList.jsx";
 import BlogPost from "./components/Blog/BlogPost.jsx";
 import BlogEditor from "./components/Blog/BlogEditor.jsx";
 import UserBlogDashboard from "./components/Blog/UserBlogDashboard.jsx";
 
-// Import the Home component
-import Home from "./components/home/Home.jsx"; // Make sure the path is correct
+// Home
+import Home from "./components/home/Home.jsx";
+import Profile from "./components/ProfilePage/profile.jsx";
+import AboutSite from "./components/aboutUs/AboutSite.jsx";
 
 function App() {
   return (
-      <Routes>
-        {/* Main Site wrapped in MainLayout */}
-        <Route
-          path="/"
-          element={
-            <MainLayout>
-              <Home />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <MainLayout>
-              <AboutUs />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/contact"
-          element={
-            <MainLayout>
-              <ContactUs />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/explore"
-          element={
-            <MainLayout>
-              <Explore />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/explore/bikespecs/:id"
-          element={
-            <MainLayout>
-              <BikeSpecs />
-            </MainLayout>
-          }
-        />
-        
-        {/* Blog Routes */}
-        <Route
-          path="/blog"
-          element={
-            <MainLayout>
-              <BlogList />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/blog/:slug"
-          element={
-            <MainLayout>
-              <BlogPost />
-            </MainLayout>
-          }
-        />
-        
-        {/* Auth Routes */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        
-        {/* Protected Routes */}
-        <Route
-          path="/user-dashboard"
-          element={
-            <ProtectedRoute role="user">
-              <MainLayout>
-                <UserDashboard />
-              </MainLayout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin-dashboard"
-          element={
-            <ProtectedRoute role="admin">
-              <MainLayout>
-                <AdminDashboard />
-              </MainLayout>
-            </ProtectedRoute>
-          }
-        />
+    <Routes>
+      {/* Main Site wrapped in MainLayout */}
+      <Route
+        path="/"
+        element={
+          <MainLayout>
+            <Home />
+            <Profile />
+            <AboutSite />
+          </MainLayout>
+        }
+      />
+      <Route
+        path="/about"
+        element={
+          <MainLayout>
+            <AboutUs />
+          </MainLayout>
+        }
+      />
+      <Route
+        path="/contact"
+        element={
+          <MainLayout>
+            <ContactUs />
+          </MainLayout>
+        }
+      />
+      <Route
+        path="/explore"
+        element={
+          <MainLayout>
+            <Explore />
+          </MainLayout>
+        }
+      />
+      <Route
+        path="/explore/bikespecs/:id"
+        element={
+          <MainLayout>
+            <BikeSpecs />
+          </MainLayout>
+        }
+      />
 
-        {/* Admin Dashboard - PROTECTED ROUTES */}
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute role="admin">
-              <AdminLayout />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/add-bike"
-          element={
-            <ProtectedRoute role="admin">
-              <AddBike />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/all-bikes"
-          element={
-            <ProtectedRoute role="admin">
-              <AllBikes />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/edit-bike/:id"
-          element={
-            <ProtectedRoute role="admin">
-              <EditBike />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/users"
-          element={
-            <ProtectedRoute role="admin">
-              <Users />
-            </ProtectedRoute>
-          }
-        />
-        
-        {/* Protected Blog Routes */}
-        <Route
-          path="/blog/edit/:id?"
-          element={
-            <ProtectedRoute>
-              <MainLayout>
-                <BlogEditor />
-              </MainLayout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/user/blog"
-          element={
-            <ProtectedRoute role="user">
-              <MainLayout>
-                <UserBlogDashboard />
-              </MainLayout>
-            </ProtectedRoute>
-          }
-        />
+      {/* Blog Routes */}
+      <Route
+        path="/blog"
+        element={
+          <MainLayout>
+            <BlogList />
+          </MainLayout>
+        }
+      />
+      <Route
+        path="/blog/:slug"
+        element={
+          <MainLayout>
+            <BlogPost />
+          </MainLayout>
+        }
+      />
 
-        {/* 404 */}
-        <Route
-          path="*"
-          element={
+      {/* Auth Routes */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* Protected Routes */}
+      <Route
+        path="/user-dashboard"
+        element={
+          <ProtectedRoute role="user">
             <MainLayout>
-              <h1 className="text-center mt-10">404 - Page Not Found</h1>
+              <UserDashboard />
             </MainLayout>
-          }
-        />
-      </Routes>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin-dashboard"
+        element={
+          <ProtectedRoute role="admin">
+            <MainLayout>
+              <AdminDashboard />
+            </MainLayout>
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Admin Dashboard */}
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute role="admin">
+            <AdminLayout />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/add-bike"
+        element={
+          <ProtectedRoute role="admin">
+            <AddBike />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/all-bikes"
+        element={
+          <ProtectedRoute role="admin">
+            <AllBikes />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/edit-bike/:id"
+        element={
+          <ProtectedRoute role="admin">
+            <EditBike />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/users"
+        element={
+          <ProtectedRoute role="admin">
+            <Users />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/users/edit-user/:id"
+        element={
+          <ProtectedRoute role="admin">
+            <EditUser />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Protected Blog Routes */}
+      <Route
+        path="/blog/edit/:id?"
+        element={
+          <ProtectedRoute>
+            <MainLayout>
+              <BlogEditor />
+            </MainLayout>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/user/blog"
+        element={
+          <ProtectedRoute role="user">
+            <MainLayout>
+              <UserBlogDashboard />
+            </MainLayout>
+          </ProtectedRoute>
+        }
+      />
+
+      {/* 404 */}
+      <Route
+        path="*"
+        element={
+          <MainLayout>
+            <h1 className="text-center mt-10">404 - Page Not Found</h1>
+          </MainLayout>
+        }
+      />
+    </Routes>
   );
 }
 
